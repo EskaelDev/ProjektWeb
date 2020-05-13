e1f16210a28d5e1583cd578e1d09bf8f2903137a
natsyu <rainbowvme@gmail.com>
2020-05-13 23:06:35 +0200

    Logowanie przez jwt, pobranie aktualnie zalogowanego

---
+++ ProjektWeb/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjektWeb.Data.Entities;
+using ProjektWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProjektWeb.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected IUserService UsersService { get; private set; }
+        protected IHttpContextAccessor HttpContextAccessor { get; private set; }
+
+        public BaseController(IUserService usersService, IHttpContextAccessor httpContextAccessor)
+        {
+            UsersService = usersService;
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        protected async Task<User> GetCurrentUser()
+        {
+            var userId = HttpContextAccessor.HttpContext.User.Identity.Name;
+            return await UsersService.FindById(userId);
+        }
+    }
+}
--- ProjektWeb/Startup.cs
+++ ProjektWeb/Startup.cs
@@ -5,6 +5,12 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjektWeb.Helpers;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using ProjektWeb.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace ProjektWeb
 {
@@ -20,6 +26,38 @@ namespace ProjektWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -50,6 +88,15 @@ namespace ProjektWeb
             }
 
             app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
