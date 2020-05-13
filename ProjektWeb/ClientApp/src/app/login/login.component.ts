8283b7c65ec57866fb8c29c17228d3f04b720413
Ola Druciak <ola.druciak@wp.pl>
2020-05-13 23:08:23 +0200

    Add rest files

---
+++ ProjektWeb/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+  hide = true;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  save() {
+
+  }
+
+  getErrorMessage() {
+  }
+}
