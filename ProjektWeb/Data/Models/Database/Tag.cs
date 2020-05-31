6f5ef237ddd4856868133451f58d2bea7ffba6be
natsyu <rainbowvme@gmail.com>
2020-05-31 19:50:18 +0200

    Updated Tag model

--- ProjektWeb/Data/Models/Database/Tag.cs
+++ ProjektWeb/Data/Models/Database/Tag.cs
@@ -11,7 +11,7 @@ namespace ProjektWeb.Data.Models.Database
     {
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int TagId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int ElementId { get; set; }
     }
